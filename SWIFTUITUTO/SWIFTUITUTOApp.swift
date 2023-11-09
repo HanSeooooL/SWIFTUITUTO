54f548717fd83e9d6b83fb587fa2ce84e9bd342f
HANSEOL <nothinonthere@icoud.com>
2023-11-09 10:53:57 +0900

    Initial Commit

---
+++ SWIFTUITUTO/SWIFTUITUTOApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWIFTUITUTOApp.swift
+//  SWIFTUITUTO
+//
+//  Created by 한설 on 2023/11/09.
+//
+
+import SwiftUI
+
+@main
+struct SWIFTUITUTOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
